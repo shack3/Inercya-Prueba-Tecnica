7d375d56470fc4cd7aced8fb529ce81cd4439727
Sergio Morales <50766544+shack3@users.noreply.github.com>
2021-10-25 18:32:03 +0200

    V 0.1 - Tercer ejercicio completado.

---
+++ 3-Random/3-Random/3-Random/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+namespace _3_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int N = 100000;
+            HashSet<int> randomNumbers = new HashSet<int>();
+            Random rand = new Random();
+            string output;
+
+            string pwd = System.IO.Directory.GetCurrentDirectory();
+            pwd = Path.Combine(pwd, "numerosRandom.txt");
+
+            
+            for(int i=0; i<N; i++)
+            {
+                randomNumbers.Add(rand.Next(0, 2147483647));
+            }
+
+            output = string.Join("\n", randomNumbers);
+
+            File.WriteAllText(pwd, output);
+
+            Console.WriteLine("Archivo guardado en: " + pwd + "\n\nPulse una tecla para salir...");
+
+
+            Console.ReadKey();
+        }
+    }
+}
