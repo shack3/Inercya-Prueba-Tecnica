06e260f26a1d8ceab199534306121c35284310cd
Sergio Morales <50766544+shack3@users.noreply.github.com>
2021-10-26 13:30:40 +0200

    V 0.3.2 - Ejercicio 1 completado

---
+++ 1-SQL/1.4.sql
@@ -0,0 +1,11 @@
+WITH RECURSIVE emp
+AS(
+		SELECT EmployeeID, FirstName, LastName, ReportsTo 
+		FROM employees
+		WHERE FirstName='Andrew' AND LastName='Fuller'
+UNION ALL
+		SELECT e.EmployeeID, e.FirstName, e.LastName, e.ReportsTo 
+		FROM emp
+        INNER JOIN employees e ON emp.EmployeeID = e.ReportsTo
+)
+SELECT * FROM emp;
