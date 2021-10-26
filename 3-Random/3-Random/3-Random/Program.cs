3a7ee79b40e02a54caff4f5588e9f05e8495a4c5
Sergio Morales <50766544+shack3@users.noreply.github.com>
2021-10-26 23:54:02 +0200

    V 4.1 - Arreglado un error del ejercicio 1

--- 3-Random/3-Random/3-Random/Program.cs
+++ 3-Random/3-Random/3-Random/Program.cs
@@ -10,7 +10,7 @@ namespace _3_Random
     {
         static void Main(string[] args)
         {
-            
+
             int N = 100000;
             HashSet<int> randomNumbers = new HashSet<int>();
             Random rand = new Random();
@@ -19,11 +19,12 @@ namespace _3_Random
             string pwd = System.IO.Directory.GetCurrentDirectory();
             pwd = Path.Combine(pwd, "numerosRandom.txt");
 
-            
-            for(int i=0; i<N; i++)
+
+            while (randomNumbers.Count < N)
             {
                 randomNumbers.Add(rand.Next(0, 2147483647));
             }
+
 
             output = string.Join("\n", randomNumbers);
 
